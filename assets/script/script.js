9b72512f3cd6a7f73d1c11a36b243f0004734a31
Pehoffmann <pedrob_hoffmann@outlook.com>
2021-02-15 02:02:07 -0300

    feature/02

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,47 @@
 "use strict";
 
-/*document.querySelector(".message").textContent = "🎉Número Certo!";*/
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = document.querySelector(".guess").value;
+  const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
+
   if (!guess) {
-    document.querySelector(".message").textContent =
-      "⛔ Nenhum número selecionado!";
+    displayMessage("⛔ Nenhum número selecionado!");
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Número Certo!");
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber ? "📈 Muito alto!" : "📉 Muito baixo!"
+      );
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💣 Você perdeu!");
+      document.querySelector(".score").textContent = 0;
+    }
   }
+});
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage("Comece a tentar...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#333";
+  document.querySelector(".number").style.width = "15rem";
 });
